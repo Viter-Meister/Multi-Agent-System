90607dd8b077ea51a18be689857334133fad140b
Viktor <viter02@inbox.ru>
2024-04-20 19:45:26 +0300

    Add Speech Command to PascalABC

---
+++ PascalABC/Speech.pas
@@ -0,0 +1,72 @@
+﻿uses Microsoft.Speech;
+uses Microsoft.Speech.Synthesis;
+uses Microsoft.Speech.Recognition;
+uses System.Globalization;
+uses System.Threading;
+uses Robotics, TCPServer;
+{$reference Microsoft.Speech.dll}
+{$reference System.Threading.dll}
+
+var completed : ManualResetEvent;
+var server := new ServerObj('192.168.31.132', 8888);
+var robot := new Robot('robot1');
+
+procedure speechEngine_SpeechRecognized(sender: object; e: SpeechRecognizedEventArgs);
+begin
+  if (e.Result.Text = 'вперед') then
+  begin
+    server.Send(robot.MoveForward());
+    Console.WriteLine('вперед!');
+    server.Start();
+  end;
+  if (e.Result.Text = 'назад') then
+  begin
+    server.Send(robot.MoveBack());
+    Console.WriteLine('назад!');
+    server.Start();
+  end;
+  if (e.Result.Text = 'влево') then
+  begin
+    server.Send(robot.TurnLeft());
+    Console.WriteLine('влево!');
+    server.Start();
+  end;
+  if (e.Result.Text = 'вправо') then
+  begin
+    server.Send(robot.TurnRight());
+    Console.WriteLine('вправо!');
+    server.Start();
+  end;
+  if (e.Result.Text = 'конец') then
+  begin
+    Console.WriteLine('конец!');
+    completed.Set();
+  end;
+end;
+
+begin
+  completed := new ManualResetEvent(false);
+  
+  var culture := new CultureInfo('ru-ru');
+  var speechEngine := new SpeechRecognitionEngine(culture);
+  
+  var choices := new Choices();
+  choices.Add('вперед');
+  choices.Add('назад');
+  choices.Add('влево');
+  choices.Add('вправо');
+  choices.Add('конец');
+  
+  var grammarBuilder := new GrammarBuilder();
+  grammarBuilder.Append(choices);
+  var grammar := new Grammar(grammarBuilder);
+  speechEngine.LoadGrammar(grammar);
+  
+  speechEngine.SpeechRecognized += speechEngine_SpeechRecognized; 
+  
+  speechEngine.SetInputToDefaultAudioDevice();
+  speechEngine.RecognizeAsync(RecognizeMode.Multiple);
+  
+  completed.WaitOne();
+  speechEngine.Dispose();
+end.
